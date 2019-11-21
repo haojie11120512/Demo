ad1a4dc2b8060ea9fbdbb7cedee49a90977d6c18
haojie11120512 <1085572639@qq.com>
2019-11-21 16:30:26 +0800

    第二次上传

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView aaa = new TextView(this);
+
+        aaa.setText("哈哈哈哈哈哈");
     }
 }
